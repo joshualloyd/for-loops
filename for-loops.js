cb150c5322e6906023b2a2c5e3ec356c3da12e4a
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-05-25 16:06:01 -0500

    progrees to array iteration

---
+++ for-loops.js
@@ -0,0 +1,28 @@
+// Increment by 10
+// for (var i = 5; i <= 120; i = i + 10) {
+//    console.log(i);
+// }
+
+// Decrement by Division
+// for (var i = 4096; i >= 1; i = i / 2) {
+// 	console.log(i);
+// }
+
+// Array Iteration
+
+// array of presidents
+var presidents = [
+  "George Washington",
+  "John Adams",
+  "Thomas Jefferson",
+  "James Madison",
+  "James Monroe",
+  "John Quincy Adams",
+  "Andrew Jackson",
+  "Martin Van Buren",
+  "William Henry Harrison"
+]
+//
+for (var i = 0; i < presidents.length; i++) {
+	console.log(presidents[i]);
+}
