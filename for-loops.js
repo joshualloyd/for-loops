c443f7e1ef5a30aa60eb53e7351bf10f489b9127
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-05-27 18:16:10 -0500

    finished for-loops exercise

--- for-loops.js
+++ for-loops.js
@@ -11,53 +11,72 @@
 // Array Iteration
 
 // array of presidents
-var presidents = [
-  "George Washington",
-  "John Adams",
-  "Thomas Jefferson",
-  "James Madison",
-  "James Monroe",
-  "John Quincy Adams",
-  "Andrew Jackson",
-  "Martin Van Buren",
-  "William Henry Harrison",
-  "John Tyler",
-	"James K. Polk",
-	"Zachary Taylor",
-	"Millard Fillmore",
-	"Franklin Pierce",
-	"James Buchanan",
-	"Abraham Lincoln",
-	"Andrew Johnson",
-	"Ulysses Grant",
-	"Rutherford B. Hayes",
-	"James Garfield",
-	"Chester A. Arthur",
-	"Grover Cleveland",
-	"Benjamin Harrison",
-	"Grover Cleveland",
-	"William McKinley",
-	"Theodore Roosevelt",
-	"William Taft",
-	"Woodrow Wilson",
-	"Warren Harding",
-	"Calvin Coolidge",
-	"Herbert Hoover",
-	"Franklin  Roosevelt",
-	"Harry  Truman",
-	"Dwight Eisenhower",
-	"John F. Kennedy",
-	"Lyndon Johnson",
-	"Richard Nixon",
-	"Gerald Ford",
-	"Jimmy Carter",
-	"Ronald Reagan",
-	"George H. W. Bush",
-	"Bill Clinton",
-	"George W. Bush",
-	"Barack Obama"
-]
+// var presidents = [
+//   	"George Washington",
+//   	"John Adams",
+//   	"Thomas Jefferson",
+//   	"James Madison",
+//   	"James Monroe",
+//   	"John Quincy Adams",
+//   	"Andrew Jackson",
+//   	"Martin Van Buren",
+//   	"William Henry Harrison",
+//   	"John Tyler",
+// 	"James K. Polk",
+// 	"Zachary Taylor",
+// 	"Millard Fillmore",
+// 	"Franklin Pierce",
+// 	"James Buchanan",
+// 	"Abraham Lincoln",
+// 	"Andrew Johnson",
+// 	"Ulysses Grant",
+// 	"Rutherford B. Hayes",
+// 	"James Garfield",
+// 	"Chester A. Arthur",
+// 	"Grover Cleveland",
+// 	"Benjamin Harrison",
+// 	"Grover Cleveland",
+// 	"William McKinley",
+// 	"Theodore Roosevelt",
+// 	"William Taft",
+// 	"Woodrow Wilson",
+// 	"Warren Harding",
+// 	"Calvin Coolidge",
+// 	"Herbert Hoover",
+// 	"Franklin  Roosevelt",
+// 	"Harry  Truman",
+// 	"Dwight Eisenhower",
+// 	"John F. Kennedy",
+// 	"Lyndon Johnson",
+// 	"Richard Nixon",
+// 	"Gerald Ford",
+// 	"Jimmy Carter",
+// 	"Ronald Reagan",
+// 	"George H. W. Bush",
+// 	"Bill Clinton",
+// 	"George W. Bush",
+// 	"Barack Obama"
+// ]
 // logging presidents to the console
-for (var i = 1; i <= presidents.length; i++) {
-	console.log(`President #${[i]} was ${presidents[i]}`);
-}
+// for (var i = 0; i < presidents.length; i++) {
+// 	console.log(`President #${parseInt([i]) + 1} was ${presidents[i]}`);
+// }
+
+// Object Iteration
+// var antSpecies = {
+//   argentine: {},
+//   army: {},
+//   bigHeaded: {},
+//   black: {},
+//   bull: {},
+//   carpenter: {},
+//   crazy: {},
+//   fire: {},
+//   glider: {},
+//   honeyPot: {},
+//   jackJumper: {}
+// }
+
+// for (var prop in antSpecies) {
+// 	console.log(`${prop}`);
+// }
